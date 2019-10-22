62df73e08a62d14e2ed80c319dd3a722b49a5adb
Ruben Young <40431675+KaiyenY@users.noreply.github.com>
2019-10-22 08:55:40 -0400

    Changed to LinkedList implementation for picking up/setting down runes

--- Ermo's Fire/Assets/Scripts/HUDManager.cs
+++ Ermo's Fire/Assets/Scripts/HUDManager.cs
@@ -2,93 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// HUDManager is a player-attached script that functions as a place to handle the logic for displaying 
-/// - active rune selection
-/// - radial ui logic to know where to place icons
-/// - maybe any other future UI considerations as well.
-/// </summary>
-public class HUDManager : MonoBehaviour
+namespace UnityStandardAssets._2D
 {
-    // Flag for whether we're selecting an active rune or not
-    public bool RuneSelection = true;
-
-    // How many runes and the list to hold their positions
-    // TODO: runecount is const by design at the moment, 
-    // for our goal of having a game with a flexible inventory (being able to set down or carry a rune w/ you), this must eventually be changed to allow for some sort of "abilityManager" to hold the active runes
-    // reminder: Linked Lists!
-    public const int runeCount = 3;
-    public Vector2[] l_runePositions;
-
-    // How fast to linearly interpolate for the rune icons
-    public float lerpSpeed = 25.0f;
-
-    // List of rune game objects
-    public GameObject[] l_runeSprites;
-
-    // How far from player to display rune icons
-    public float runeRadius = 4.0f;
-
-    private void Awake()
+    /// <summary>
+    /// HUDManager is a player-attached script that functions as a place to handle the logic for displaying 
+    /// - active rune selection
+    /// - radial ui logic to know where to place icons
+    /// - maybe any other future UI considerations as well.
+    /// </summary>
+    public class HUDManager : MonoBehaviour
     {
-        // Initialize rune icon positions
-        l_runePositions = new Vector2[runeCount];
+        // Pointer to the player character, used to update rune selection / active rune flag
+        PlatformerCharacter2D m_Character;
 
-        // Initialize rune sprite array
-        l_runeSprites = new GameObject[runeCount];
+        // How many runes and the list to hold their positions
+        // TODO: runecount is const by design at the moment, 
+        // for our goal of having a game with a flexible inventory (being able to set down or carry a rune w/ you), this must eventually be changed to allow for some sort of "abilityManager" to hold the active runes
+        // reminder: Linked Lists!
+        public LinkedList<GameObject> m_lRuneList;
 
-        UpdateRunePositions();
-    }
+        // How fast to linearly interpolate for the rune icons
+        public float lerpSpeed = 25.0f;
 
-    void Start()
-    {
-        // Get all child game objects into our list
-        for(int i = 0; i < runeCount; ++i)
+        // How far from player to display rune icons
+        public float runeRadius = 4.0f;
+
+        private void Awake()
         {
-            l_runeSprites[i] = this.transform.GetChild(i).gameObject;
+            // Initialize Rune List
+            m_lRuneList = new LinkedList<GameObject>();
+
+            // Grab ref to player character
+            m_Character = GetComponent<PlatformerCharacter2D>();
         }
-    }
 
-    void Update()
-    {
-        UpdateRunePositions();
-
-        // If flag enabled: 
-        // - set runes to visible
-        // - TODO: lerp from player to outer radius position
-        if (RuneSelection)
+        void Start()
         {
-            for (int i = 0; i < runeCount; ++i)
+            // Get all child game objects into our list
+            
+        }
+
+        void Update()
+        {
+            // If flag enabled: 
+            // - set runes to visible
+            // - TODO: lerp from player to outer radius position
+            if (m_Character.selectRunes)
             {
-                l_runeSprites[i].transform.position = Vector2.Lerp(l_runeSprites[i].transform.position, l_runePositions[i], lerpSpeed * Time.deltaTime);
+                int nRunes = m_lRuneList.Count;
+                LinkedListNode<GameObject> curr = m_lRuneList.First;
+                for (int i = 0; i < nRunes; ++i)
+                {
+                    float xPos = gameObject.transform.position.x + runeRadius * Mathf.Cos(Mathf.PI * (i + 1) / (nRunes + 1));
+                    float yPos = gameObject.transform.position.y + runeRadius * Mathf.Sin(Mathf.PI * (i + 1) / (nRunes + 1));
+                    Vector2 neededPosition = new Vector2(xPos, yPos);
+                    curr.Value.transform.position = Vector2.Lerp(curr.Value.transform.position, neededPosition, lerpSpeed * Time.deltaTime);
+                    curr = curr.Next;
+                }
+            }
+            else
+            {
+                ResetRunes();
             }
         }
-        else
+
+        /// <summary>
+        ///  Lerp back to the player's center
+        /// </summary>
+        private void ResetRunes()
         {
-            ResetRunes();
+            foreach (GameObject rune in m_lRuneList)
+            {
+                rune.transform.position = Vector2.Lerp(rune.transform.position, this.gameObject.transform.position, lerpSpeed * Time.deltaTime);
+            }
         }
+
     }
-
-    private void UpdateRunePositions()
-    {
-        // For each rune, calculate its radial position with regards to the player's position
-        for(int i = 0; i < runeCount; ++i)
-        {
-            float xPos = gameObject.transform.position.x + runeRadius * Mathf.Cos(Mathf.PI * (i+1) / (runeCount+1));
-            float yPos = gameObject.transform.position.y + runeRadius * Mathf.Sin(Mathf.PI * (i+1) / (runeCount+1));
-            Vector2 vec = new Vector2(xPos, yPos);
-            l_runePositions[i] = vec;
-        }
-    }
-
-    private void ResetRunes()
-    {
-        // For now, tp all runes back into the character
-
-        foreach (GameObject rune in l_runeSprites)
-        {
-            rune.transform.position = Vector2.Lerp(rune.transform.position, this.gameObject.transform.position, lerpSpeed * Time.deltaTime);
-        }
-    }
-
 }
