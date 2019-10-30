c0b5fb3bf6ad5b97e844d3bc47f49528adced6ec
Christopher Lawrence (RIT Student) <cml3051@ad.rit.edu>
2019-10-30 01:03:56 -0400

    Polishing
    Polished level, added a bunch of stuff

---
+++ Ermo's Fire/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Update the current game state
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if ((int)GameStats.currentState != sceneIndex)
+        {
+            GameStats.currentState = (GameState)sceneIndex;
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Current State is " + GameStats.currentState.ToString());
+        switch (GameStats.currentState)
+        {
+            case GameState.Title:
+                // Title -> Introduction: Press Enter key
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    GameStats.currentState = GameState.Introduction;
+                    SceneManager.LoadScene("Introduction");
+                }
+                break;
+            case GameState.Introduction:
+                GameObject checkpoint = GameObject.Find("Checkpoint");
+                Vector2 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+                if (checkpoint != null && Mathf.Abs(playerPos.x - checkpoint.transform.position.x) < 2f)
+                {
+                    GameStats.currentState = GameState.SnowScene;
+                    SceneManager.LoadScene("SnowScene");
+                }
+                break;
+            case GameState.SnowScene:
+                break;
+            case GameState.WaterScene:
+                break;
+            case GameState.Pause:
+                break;
+            case GameState.Lose:
+                break;
+            case GameState.Win:
+                break;
+        }
+    }
+}
---
+++ Ermo's Fire/Assets/Scripts/GameStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Title,
+    Introduction,
+    SnowScene,
+    WaterScene,
+    Lose,
+    Win,
+    Pause
+}
+
+public class GameStats : MonoBehaviour
+{
+    public static GameState currentState = GameState.Title;
+
+}
